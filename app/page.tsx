2277cf47f853fd26e8496b1e90ce9c4189710d79
Theo Rodriguez <rodriguez.theo@outlook.com>
2023-10-28 14:38:45 +0200

    Adding next/image

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import AcmeLogo from "@/app/ui/acme-logo";
 
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -25,10 +29,25 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span>
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
